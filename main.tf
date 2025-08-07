13dccccc70b8e7e9f3e73633640ec7cdc4922b12
Eduardo Demeneck Onghero <acrackintheice@gmail.com>
2025-08-07 15:51:12 +0000

    adds things

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
-resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
+terraform {
+  backend "gcs" {
+    bucket = "test_bucket_eonghero"
+    prefix = "terraform/state/tm-test"
+  }
+
+  required_version = "1.5.7"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.47.0"
+    }
+  }
 }
 
-output "random_string" {
-    value = random_string.random.id
+resource "google_service_account" "vault-server" {
+  account_id   = "eonghero-tm-test"
+  display_name = "eonghero-tm-test"
+  project      = "qt-msa-sandbox-7b"
 }
